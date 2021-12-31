326e5a44f2c676bf3669bda6f0b71090db279d88
Michael Turmon <turmon@jpl.nasa.gov>
2021-12-30 17:55:45 -0800

    update html-serve, streamline and remove old fifo shutdown mechanism

--- util/html-serve.sh
+++ util/html-serve.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #
 # html-serve.sh: start/stop HTTP server
 #
 # This starts or stops the HTTP server that serves up the Exosims
 # table of contents and ensemble-summary files.
-# We run the server on a non-standard port - 8090 by default.  So,
-# to see the table of contents, navigate to:
-#  http://aftac1.jpl.nasa.gov:8090/sims/
+#
+# We run the server on a non-standard port - 8090 by default.
+# To see the table of contents, navigate to:
+#   http://mustang2.jpl.nasa.gov:8090/sims/
 #
 # Usage:
 #   html-serve.sh [-h] [-p PORT] MODE
@@ -21,15 +22,17 @@
 #  -p PORT => gives the HTTP port number.  Default is 8090.
 #  -h      => shows this help text.
 #
-# Implementation note: Status files are kept in files called:
-#   Local/www-service/http.PORT.*
+# Implementation:
+# -- Status files are kept in these files:
+#   Local/www-service/http-HOST.PORT.*
 # where PORT is typically 8090.  (Formerly 8088.)
-# There is a server error log, a process ID of the server, and a fifo that
-# is used to signal the running server to exit.  If the basic "stop" 
-# MODE of this command does not work, you can kill the server by the
-# process ID (pid) in that directory.
+# There is a server error log (.log) and a file containing the process ID (PID)
+# of the server (.pid).
+# The PID is used to signal the running server to exit.  If the basic "stop" 
+# MODE of this command does not work, you can kill the server by sending TERM
+# to process ID (pid) named in that file.
 # 
-# turmon sep 2018
+# turmon sep 2018, dec 2021
 #
 ## [end comment block]
 
@@ -90,30 +93,26 @@ fi
 
 mode="$1"
 
+host=$(hostname -s)
+
 # specific filenames we need
-SERVER_FIFO=$SERVER_DIR/http.$port.fifo
-SERVER_LOG=$SERVER_DIR/http.$port.log
-SERVER_PID=$SERVER_DIR/http.$port.pid
+SERVER_LOG=$SERVER_DIR/http-$host.$port.log
+SERVER_PID=$SERVER_DIR/http-$host.$port.pid
 
 
 # set up the server state dir
 function setup {
     # (assume it is already created and has right mode)
-    # mkdir -m 775 -p "$SERVER_DIR"
-    # chgrp $SERVER_GROUP "$SERVER_DIR"
-    # re-create fifo
-    rm -f "$SERVER_FIFO"
-    mkfifo -m 664 "$SERVER_FIFO"
-    chgrp $SERVER_GROUP "$SERVER_FIFO"
-    # truncate and re-create server log
+    ## mkdir -m 775 -p "$SERVER_DIR"
+    ## chgrp $SERVER_GROUP "$SERVER_DIR"
+    # copy and reset the server log
+    [ -f "$SERVER_LOG" ] && cp -pf "$SERVER_LOG" "$SERVER_LOG".old
     rm -f "$SERVER_LOG" && touch "$SERVER_LOG"
     echo "${PROGNAME}: HTTP file setup OK." >> "$SERVER_LOG"
     chgrp $SERVER_GROUP "$SERVER_LOG"
     chmod 664 "$SERVER_LOG"
-    # truncate server PID file
-    rm -f "$SERVER_PID" && touch "$SERVER_PID"
-    chgrp $SERVER_GROUP "$SERVER_PID"
-    chmod 664 "$SERVER_PID"
+    # remove server PID file, if any
+    rm -f "$SERVER_PID"
 }
 
 
@@ -121,11 +120,11 @@ if [ "$mode" == start ]; then
     #
     # start the HTTP server
     #
-    # Try not to have two going on same port!
-    if [ -r $SERVER_FIFO ]; then
+    # Try not to have two going on same machine + port!
+    if [ -r $SERVER_PID ]; then
 	echo "${PROGNAME}: Server may be running on $port already." >&2
 	echo "${PROGNAME}: Attempting to shut down cleanly and restart." >&2
-	echo > $SERVER_FIFO &
+	kill -TERM $(cat $SERVER_PID)
 	sleep 0.1
 	# if the PID file was removed, that's a good sign
 	if [ -r "$SERVER_PID" ]; then
@@ -140,7 +139,7 @@ if [ "$mode" == start ]; then
     if true; then
 	# apache httpd - multi-threaded, serves movies properly
 	# minimal config, PID and logging configured on command line
-	httpd -f "$CURR_DIR/$SERVER_DIR/httpd.conf" \
+	httpd -f "$CURR_DIR/$SERVER_DIR/config/httpd-apache2.4.conf" \
 	      -c "PidFile $CURR_DIR/$SERVER_PID" \
 	      -c "ErrorLog $CURR_DIR/$SERVER_LOG" \
 	      -c "Listen $port"
@@ -150,101 +149,49 @@ if [ "$mode" == start ]; then
 	# python SimpleHTTPServer: single-threaded, does not support returning
 	# byte ranges, which makes .mp4s not render in Safari.
 	# mostly-daemonized: nohup, stdout to /dev/null, sterrr to logfile
+	# (12/2021: this has not been tested recently)
 	nohup python -m SimpleHTTPServer "$port" 1>/dev/null 2>>$SERVER_LOG &
 	# capture server PID - last backgrounded command
 	server_pid_var=$!
     fi
-    # last status update to stdout
+    # status update to stdout
     [ $port != $DEFAULT_PORT ] && p_text="-p $port" || p_text=""
     echo "${PROGNAME}: Started the server.  Stop with: \`${PROGNAME} $p_text stop'" 
-    # this is just informational, the correct way to kill is with the FIFO
-    echo $server_pid_var >> $SERVER_PID
-    # mostly-daemonize ourselves
-    trap "" SIGHUP # ignore exit of parent shell
-    exec >/dev/null
-    exec 2>/dev/null
-    exec 0</dev/null
-    # wait for a message on the FIFO, kill the server when received, clean up.
-    # all done in background, so we can terminate this command.
-    { read < $SERVER_FIFO; 
-      echo "${PROGNAME}: Killing the HTTP server at $port" >> $SERVER_LOG;
-      kill "$server_pid_var";
-      rm -f $SERVER_FIFO $SERVER_PID; } &
-elif [ "$mode" == new ]; then
-    #
-    # start the HTTP server
-    #
-    # Try not to have two going on same port!
-    if [ -r $SERVER_FIFO ]; then
-	echo "${PROGNAME}: Server may be running on $port already." >&2
-	echo "${PROGNAME}: Attempting to shut down cleanly and restart." >&2
-	echo > $SERVER_FIFO &
-	sleep 0.1
-	# if the PID file was removed, that's a good sign
-	if [ -r "$SERVER_PID" ]; then
-	    echo "${PROGNAME}: Could not shut down cleanly." >&2
-	else
-	    echo "${PROGNAME}: Shut down cleanly.  Restarting." >&2
-	fi
-    fi
-    # reset the file status
-    setup
-    # apache httpd, minimal config, PID and logging configured on command line
-    httpd -f "$CURR_DIR/httpd.conf" \
-	  -c "PidFile $CURR_DIR/$SERVER_PID" \
-	  -c "ErrorLog $CURR_DIR/$SERVER_LOG" \
-	  -c "Listen $port"
-    # capture server PID -- httpd forks, so the pid changes
-    server_pid_var=$(sleep 0.1 && cat "$CURR_DIR/$SERVER_PID")
-    # last status update to stdout
-    [ $port != $DEFAULT_PORT ] && p_text="-p $port" || p_text=""
-    echo "${PROGNAME}: Started the server.  Stop with: \`${PROGNAME} $p_text stop'" 
-    # this is just informational, the correct way to kill is with the FIFO
-    echo $server_pid_var >> $SERVER_PID
-    # mostly-daemonize ourselves
-    trap "" SIGHUP # ignore exit of parent shell
-    exec >/dev/null
-    exec 2>/dev/null
-    exec 0</dev/null
-    # wait for a message on the FIFO, kill the server when received, clean up.
-    # all done in background, so we can terminate this command.
-    { read < $SERVER_FIFO; 
-      kill "$server_pid_var";
-      echo "${PROGNAME}: Killing the HTTP server at $port" >> $SERVER_LOG;
-      rm -f $SERVER_FIFO $SERVER_PID; } &
+
 elif [ "$mode" == stop ]; then
     #
     # stop the HTTP server
     #
-    if [ ! -r $SERVER_FIFO ]; then
-	echo "${PROGNAME}: No server is running on $port." >&2
+    if [ ! -r $SERVER_PID ]; then
+	echo "${PROGNAME}: No server is running on $host on port number $port." >&2
 	echo "${PROGNAME}: Try \`${PROGNAME} status', or check $SERVER_DIR for another port number?" >&2
 	exit 1
     fi
-    echo "${PROGNAME}: Killing server at $SERVER_FIFO." 
-    echo > $SERVER_FIFO
-elif [ "$mode" == init ]; then
-    #
-    # ensure that files can be set up -- not necessary
-    #
-    setup
-    # don't want the files hanging round...
-    # but this means they could be created OK
-    rm -f $SERVER_LOG $SERVER_FIFO $SERVER_PID
-    echo "${PROGNAME}: Initialized OK (see $SERVER_DIR)." 
+    echo "${PROGNAME}: Killing server at $SERVER_PID."
+    kill -TERM $(cat $SERVER_PID)
+
 elif [ "$mode" == status ]; then
     #
     # which servers are running?
     #
-    filepat="$SERVER_DIR/*.pid"
+    filepat="$SERVER_DIR/http-*.pid"
     if ls $filepat &> /dev/null; then
 	echo "Found server(s):" 
 	for pfile in $filepat; do
+	    echo "$pfile"
 	    p_info=$(stat -c 'started by %U on %y' $pfile)
-	    p_num=$(echo $pfile | sed -e 's|.*/http\.||' -e 's|\..*||')
-	    echo "  " "PORT=$p_num" " -- " "$p_info"
+	    x_hopo=$(echo $pfile | sed -e 's|.*/http-||' -e 's|\.pid||' -e 's|\.|:|')
+	    x_host=$(echo $x_hopo | sed -e 's|:.*||')
+	    x_port=$(echo $x_hopo | sed -e 's|.*:||')
+	    echo "  " "HOST:PORT = $x_hopo"
+	    echo "  " "$p_info"
+	    echo "  " "View at this URL: http://${x_host}.jpl.nasa.gov:${x_port}/sims"
+	    if wget -q -t 1 -O /dev/null "http://${x_host}.jpl.nasa.gov:${x_port}/sims/"; then
+		echo "  " "Server seems to respond OK."
+	    else
+		echo "  " "Server does not respond."
+	    fi
 	done
-	echo "Should be able to view at http://$HOST:PORT/sims"
     else
 	echo "No HTTP servers running." 
     fi
